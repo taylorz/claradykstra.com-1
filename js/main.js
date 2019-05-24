28816ee200135242f0b8a564c46c3e2661eaa3e9
taylorz <taylor.zanke@gmail.com>
2019-05-23 21:06:26 -0400

    slick captions

--- js/main.js
+++ js/main.js
@@ -7,16 +7,22 @@ $('document').ready(function(){
     centerMode: true,
     centerPadding: '15%',
 	  infinite: false,
-	  speed: 5000,
+	  speed: 250,
 	  slidesToShow: 1,
 	  easing: 'easeInOut',
-  
-
-    // fade: true,
-    // focusOnSelect: true,
-    // arrows: false,
-
 	  prevArrow: $('.prev'),
 	  nextArrow: $('.next')
+	});
+  $('.the-carousel-captions').slick({
+	  dots: false,
+    centerMode: true,
+    centerPadding: '15%',
+	  infinite: false,
+	  speed: 250,
+	  slidesToShow: 1,
+	  easing: 'easeInOut',
+	  prevArrow: $('.prev'),
+	  nextArrow: $('.next'),
+    asNavFor: '.the-carousel'
 	});
 });
